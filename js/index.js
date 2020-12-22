59f95b90c5fee999df9e3c8e3a11334cd146e36c
Yaşar Çakır <yasar@Yasar-MacBook-Pro.local>
2020-12-22 22:02:07 +0300

    get post added refresh

--- js/index.js
+++ js/index.js
@@ -2,17 +2,45 @@ const addForm = document.querySelector('.add')
 const addInput = document.querySelector('.add-input')
 const list = document.querySelector('.list')
 const editBtn = document.querySelector('.edit-btn')
+const http = 'http://localhost:5000'
 
-addForm.addEventListener('submit', e => {
+addEventListener('DOMContentLoaded', async () => {
+	try {
+		const response = await fetch(http)
+		const data = await response.json()
+		console.log(data)
+		data.map(item => {
+			list.innerHTML += `<li class="list-item">
+		<span class="text">${item.body}</span>
+		<span class="edit"><i class="far fa-edit"></i></span
+		><span class="delete"><i class="fas fa-trash"></i></span>
+	</li>`
+		})
+	} catch (err) {
+		console.log(err.message)
+	}
+	addInput.focus()
+})
+
+addForm.addEventListener('submit', async e => {
 	e.preventDefault()
-	list.innerHTML += `
-<li class="list-item">
-						<span class="text">${addInput.value.trim()}</span>
-						<span class="edit"><i class="far fa-edit"></i></span
-						><span class="delete"><i class="fas fa-trash"></i></span>
-					</li>
-`
-	addInput.value = ''
+	const value = addInput.value.trim()
+	console.log(JSON.stringify(value))
+	try {
+		const res = await fetch(http, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ body: value }),
+		})
+		const data = await res.json()
+		if (data) {
+			location.reload()
+		}
+
+		console.log(data)
+	} catch (err) {
+		console.log(err.message)
+	}
 })
 
 list.addEventListener('click', e => {
