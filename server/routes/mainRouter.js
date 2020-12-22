5c3a251be85c65e93ade73fddc57d763f1526432
Yaşar Çakır <yasar@Yasar-MacBook-Pro.local>
2020-12-22 22:00:44 +0300

    get post added

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -7,8 +7,7 @@ const { pool } = require('../db')
 router.get('/', async (req, res) => {
 	try {
 		const response = await pool.query('SELECT * FROM todos')
-		console.log(response.rows)
-		res.status(200).send(response.rows)
+		res.status(200).json(response.rows)
 	} catch (err) {
 		console.log(err.message)
 	}
@@ -16,11 +15,14 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
 	try {
-		const response = await pool.query(
-			'INSERT INTO todos(body) VALUES ($1) RETURNING *',
-			[req.body.body]
-		)
-		res.status(201).send(response.rows)
+		if(req.body.body !== '') {
+			const response = await pool.query(
+				'INSERT INTO todos(body) VALUES ($1) RETURNING *',
+				[req.body.body]
+			)
+			console.log(req.body.body)
+			res.status(201).json(response.rows[0])
+		}
 	} catch (err) {
 		console.log(err.message)
 	}
@@ -28,9 +30,23 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
 	try {
-		await pool.query('DELETE FROM todos WHERE id=$1', [req.params.id])
-		res.status(204)
+		await pool.query('DELETE FROM todos WHERE id=$1 returning *', [
+			req.params.id,
+		])
+		res.status(204).send('Successfully Deleted')
 		console.log('Successfully deleted')
+	} catch (err) {
+		console.log(err.message)
+	}
+})
+
+router.patch('/:id', async (req, res) => {
+	try {
+		const response = await pool.query('UPDATE todos SET body=$1 WHERE id=$2', [
+			req.body.body,
+			req.params.id,
+		])
+		res.status(200).send(response.rows[0])
 	} catch (err) {
 		console.log(err.message)
 	}
